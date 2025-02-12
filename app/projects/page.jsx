c82288858ac86cadff72cefaf18b98d8224d85f4
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-02-12 19:21:54 +0000

    Initial Commmit

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,15 @@ import Button from "../components/Button";
 
 export const metadata = { title: "Projects" };
 
-const projects = [
+const personalProjects = [
+  {
+    name: "Pesquisa por Código Postal",
+    url: "https://gps-bmmp.netlify.app/",
+  },
+  { name: "HedgehogPT", url: "https://hedgehogpt.netlify.app/" },
+];
+
+const courseProjects = [
   { name: "Ominifood", url: "https://omnifood-bmmp.netlify.app/" },
   { name: "Natours", url: "https://natours-bmmp.netlify.app/" },
   { name: "trillo", url: "https://trillo-bmmp.netlify.app/" },
@@ -18,10 +26,22 @@ export default function page() {
   return (
     <div className="max-w-[820px] m-auto">
       <h1 className="text-orange-400 font-bold text-2xl mb-8">
+        Personal Projects
+      </h1>
+      <ul className="flex flex-col gap-3 font-semibold mb-8">
+        {personalProjects.map(({ name, url }) => (
+          <li className="flex justify-between items-center" key={name}>
+            {name}
+            <Button url={url}>Visite site &rarr;</Button>
+          </li>
+        ))}
+      </ul>
+
+      <h1 className="text-orange-400 font-bold text-2xl mb-8">
         Course Projects
       </h1>
       <ul className="flex flex-col gap-3 font-semibold">
-        {projects.map(({ name, url }) => (
+        {courseProjects.map(({ name, url }) => (
           <li className="flex justify-between items-center" key={name}>
             {name}
             <Button url={url}>Visite site &rarr;</Button>
