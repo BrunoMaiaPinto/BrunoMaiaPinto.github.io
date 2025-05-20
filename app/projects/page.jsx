b852fc74275bcefd667952bc1ccfb764ecbb5182
BrunoMaiaPinto <brunommpinto@sapo.pt>
2025-05-20 16:15:14 +0100

    Refactor code structure for improved readability and maintainability

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,15 +1,29 @@
 import Button from "../components/Button";
+import Image from "next/image";
+import sgpt from "@/public/securepass.png";
+import gps from "@/public/codigop.png";
+import hedge from "@/public/hedgehog.png";
+import radar from "@/public/rad.png";
 
 export const metadata = { title: "Projects" };
 
 const personalProjects = [
-  { name: "SecurePassGen", url: "https://securepassgen-web.netlify.app/" },
+  {
+    name: "SecurePassGen",
+    url: "https://securepassgen-web.netlify.app/",
+    thumb: sgpt,
+  },
   {
     name: "Pesquisa por Código Postal",
     url: "https://gps-bmmp.netlify.app/",
+    thumb: gps,
   },
-  { name: "HedgehogPT", url: "https://hedgehogpt.netlify.app/" },
-  { name: "Radar Imóvel", url: "https://brunomaiapinto.pythonanywhere.com/" },
+  { name: "HedgehogPT", url: "https://hedgehogpt.netlify.app/", thumb: hedge },
+  {
+    name: "Radar Imóvel",
+    url: "https://brunomaiapinto.pythonanywhere.com/",
+    thumb: radar,
+  },
 ];
 
 const courseProjects = [
@@ -30,11 +44,19 @@ export default function page() {
       <h1 className="text-orange-400 font-bold text-2xl mb-8">
         Personal Projects
       </h1>
-      <ul className="flex flex-col gap-3 font-semibold mb-8">
-        {personalProjects.map(({ name, url }) => (
-          <li className="flex justify-between items-center" key={name}>
-            {name}
-            <Button url={url}>Visite site &rarr;</Button>
+      <ul className="md:grid grid-cols-2 gap-6 font-semibold mb-8">
+        {personalProjects.map(({ name, url, thumb }) => (
+          <li key={name}>
+            <Image
+              src={thumb}
+              alt="SecurePassGen"
+              className="object-cover object-top rounded-lg"
+              quality={40}
+            />
+            <div className="flex justify-between items-center mt-2 mb-2 ">
+              {name}
+              <Button url={url}>Visite site &rarr;</Button>
+            </div>
           </li>
         ))}
       </ul>
@@ -53,3 +75,14 @@ export default function page() {
     </div>
   );
 }
+
+/*
+<ul className="flex flex-col gap-3 font-semibold mb-8">
+        {personalProjects.map(({ name, url }) => (
+          <li className="flex justify-between items-center" key={name}>
+            {name}
+            <Button url={url}>Visite site &rarr;</Button>
+          </li>
+        ))}
+      </ul>
+      */
